58e97921c9650c7be04de798eb04963739674527
basura-persistent <57583373+basura-persistent@users.noreply.github.com>
2021-01-08 09:07:20 -0800

    Update auth_user.js

--- routes/auth_user.js
+++ routes/auth_user.js
@@ -1,10 +1,12 @@
+const express= require("express");
+const authUSERRoute = express.Router();
+
+
+const Archive_Controller = require('../controllers/arhive_Controller');
 const Authuser_Controller = require('../controllers/auth_userController');
 
-const authUSERRoute = require('koa-router')({
-    prefix: '/'
-});
 
-authUSERRoute.get('/', (req, res) => res.render('pages/index'));
+
 authUSERRoute.post('/auth_admin', Authuser_Controller.auth_admin);
 authUSERRoute.post('auth_gen', Authuser_Controller.auth_general);
 
