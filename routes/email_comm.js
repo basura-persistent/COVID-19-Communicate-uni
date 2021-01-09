a18b7a105513aa2bb2a0313f74d5f6036c4fdb48
basura-persistent <57583373+basura-persistent@users.noreply.github.com>
2021-01-09 14:10:14 -0800

    Update email_comm.js

--- routes/email_comm.js
+++ routes/email_comm.js
@@ -4,7 +4,6 @@ const email_router = express.Router();
 
 const email_controller = require('../controllers//email_controller');
 
-const email_router = require('koa-router')({
-});
+
 
 email_router.post('/input', email_controller.campus_update);//email blast using email controller class
